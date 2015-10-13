16e3cab6e5d6a0a9a0ee8d31edd133ac0a356b3e
Praveen Kumar <prkumar@redhat.com>
2015-10-13 18:36:04 +0530

    Fix install section

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
-#!/bin/sh.git
+#!/bin/sh
 
+echo "Creating bundle directory"
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 
-git clone https://github.com/VundleVim/Vundle.vim.git.git
+echo "Install youcomleteme dependencies"
+sudo dnf install gcc gcc-c++ python-devel cmake
+
+echo "Cloning plugin repo"
+git clone https://github.com/VundleVim/Vundle.vim.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/Valloric/YouCompleteMe.git
@@ -13,4 +18,9 @@ git clone https://github.com/sjl/gundo.vim.git
 git clone https://github.com/altercation/solarized.git
 git clone https://github.com/jnwhiteh/vim-golang.git
 git clone https://github.com/tfnico/vim-gradle.git
-git clone https://github.com/ShowTrailingWhitespace.git
+git clone https://github.com/elzr/vim-json.git
+
+echo "Compile YouCompleteMe"
+cd YouCompleteMe
+git submodule update --init --recursive
+python install.py
